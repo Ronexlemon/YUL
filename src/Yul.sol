cf53729673aa4844512169655bf32ae236949d51
RonexLemon <ronexondimu@gmail.com>
2024-11-13 15:21:43 +0300

    yul:condition

--- src/Yul.sol
+++ src/Yul.sol
@@ -11,4 +11,22 @@ contract Yul{
 
         }
     }
+
+    //conditional
+
+   function yul_condition(uint256 _num) public pure returns (uint256 _result) {
+    assembly {
+        
+        // Check if _num is non-zero
+        if _num {
+            _result := add(_num, 1)
+        } 
+        // Otherwise, set _result to 0
+        if iszero(_num) {
+            _result := 0
+        }
+    }
 }
+
+}
+
--- test/Yul_variable.t.sol
+++ test/Yul_variable.t.sol
@@ -23,4 +23,26 @@ contract YulTest is Test {
         assertEq(z,200);       
               
     }
+    //conditional teting
+
+    function test_condition()public{
+    uint256  _result = yul.yul_condition(1);
+        console.log(_result);
+        assertEq(_result, 2);
+    }
+    //conditio test fuzz
+    function testFuzz_condition(uint256 x)public{
+        uint256  _result = yul.yul_condition(x);
+        console.log(_result);
+      uint256 expected;
+    if (x == type(uint256).max) {
+        expected = 0; // Since adding 1 to max uint256 will overflow and we expect 0 as per the function’s logic
+    } else if (x != 0) {
+        expected = x + 1;
+    } else {
+        expected = 0;
+    }
+        assertEq(_result, expected);
+        
+    }
 }
