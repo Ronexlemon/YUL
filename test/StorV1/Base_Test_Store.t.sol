cc981b78587d60db1385e60cb05198fb6325b301
RonexLemon <ronexondimu@gmail.com>
2025-01-26 20:19:32 +0300

    base test

---
+++ test/StorV1/Base_Test_Store.t.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.2;
+
+import {Store} from "../../src/store/Store.sol";
+import {Test,console2} from "forge-std/Test.sol";
+abstract contract Base_TestV1_Store is Test{
+    Store public store;
+ function setUp()public virtual{
+    store = new Store();
+ }
+    
+function test_readValue()public view{
+    uint256 val = store.getNumberOfItems();
+    assertEq(val, 0);
+}
+}
---
+++ test/StorV1/StoreHuff.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2 ;
+
+
+import {Base_TestV1_Store} from "./Base_Test_Store.t.sol";
+
+
+contract StoreSolcHuff is Base_TestV1_Store{
+    function setUp() public override{
+        
+    }
+    
+
+}
