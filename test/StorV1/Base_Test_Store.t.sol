1b0a6060b9fd4643db51dad1fc54d1cb6adf18a9
RonexLemon <ronexondimu@gmail.com>
2025-01-26 20:31:31 +0300

    update:test

--- test/StorV1/Base_Test_Store.t.sol
+++ test/StorV1/Base_Test_Store.t.sol
@@ -13,4 +13,9 @@ function test_readValue()public view{
     uint256 val = store.getNumberOfItems();
     assertEq(val, 0);
 }
+function test_setNumber()public{
+    store.updateNumberOfItems(10);
+    uint256 val = store.getNumberOfItems();
+    assertEq(val, 10);
+}
 }
