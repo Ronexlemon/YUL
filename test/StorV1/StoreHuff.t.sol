4a68396fb9f410dcf3ab3bdad5250edfaf477455
RonexLemon <ronexondimu@gmail.com>
2025-01-26 20:29:14 +0300

    huff test

--- test/StorV1/StoreHuff.t.sol
+++ test/StorV1/StoreHuff.t.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.2 ;
 
 
-import {Base_TestV1_Store} from "./Base_Test_Store.t.sol";
+import {Base_TestV1_Store,Store} from "./Base_Test_Store.t.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
 
 contract StoreSolcHuff is Base_TestV1_Store{
+    string public constant STORE_HUFF_LOCATION = "store/Store";
     function setUp() public override{
+        store = Store( HuffDeployer.config().deploy(STORE_HUFF_LOCATION));
         
     }
     
